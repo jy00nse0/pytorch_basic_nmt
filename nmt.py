2a98a894d726d6fe50c26915f74c47c6eecc33ec
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-01-22 18:48:28 +0900

    Update nmt.py

--- nmt.py
+++ nmt.py
@@ -85,11 +85,11 @@ class NMT(nn.Module):
 
         # attention: dot product attention
         # project source encoding to decoder rnn's state space
-        self.att_src_linear = nn.Linear(hidden_size * 2, hidden_size, bias=False)
+        self.att_src_linear = nn.Linear(hidden_size, hidden_size, bias=False)
 
         # transformation of decoder hidden states and context vectors before reading out target words
         # this produces the `attentional vector` in (Luong et al., 2015)
-        self.att_vec_linear = nn.Linear(hidden_size * 2 + hidden_size, hidden_size, bias=False)
+        self.att_vec_linear = nn.Linear(hidden_size + hidden_size, hidden_size, bias=False)
 
         # prediction layer of the target vocabulary
         self.readout = nn.Linear(hidden_size, len(vocab.tgt), bias=False)
@@ -98,7 +98,7 @@ class NMT(nn.Module):
         self.dropout = nn.Dropout(self.dropout_rate)
 
         # initialize the decoder's state and cells with encoder hidden states
-        self.decoder_cell_init = nn.Linear(hidden_size * 2, hidden_size)
+        self.decoder_cell_init = nn.Linear(hidden_size, hidden_size)
 
         self.label_smoothing = label_smoothing
         if label_smoothing > 0.:
@@ -184,12 +184,13 @@ class NMT(nn.Module):
         src_encodings, (last_state, last_cell) = self.encoder_lstm(packed_src_embed)
         src_encodings, _ = pad_packed_sequence(src_encodings)
 
-        # (batch_size, src_sent_len, hidden_size * 2)
+        # (batch_size, src_sent_len, hidden_size * 1)
         src_encodings = src_encodings.permute(1, 0, 2)
 
-        dec_init_cell = self.decoder_cell_init(torch.cat([last_cell[0], last_cell[1]], dim=1))
-        dec_init_state = torch.tanh(dec_init_cell)
-
+        #dec_init_cell = self.decoder_cell_init(torch.cat([last_cell[0], last_cell[1]], dim=1))
+        dec_init_cell = last_cell
+        # dec_init_state = torch.tanh(dec_init_cell)
+        dec_init_state = last_state
         return src_encodings, (dec_init_state, dec_init_cell)
 
     def decode(self, src_encodings: torch.Tensor, src_sent_masks: torch.Tensor,
