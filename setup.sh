cd06daf15d5be76322e0ac27a5e95e623b698e2b
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-01-24 00:37:42 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+git clone https://github.com/jy00nse0/pytorch_basic_nmt.git
+cd pytorch_basic_nmt
+conda env create -f environment.yml
+cd data
+pip install gdown
+gdown --fuzzy https://drive.google.com/file/d/17bmdohiXGQDd6DE1pDl0VvNV702K8uuG/view?usp=sharing
+tar -xvf wmt14_tok_len50.tar
+
+conda activate pytorch_nmt
+chmod +x train.sh
+
+#tmux new-session -d -s nmt_train \
+#"bash scripts/train.sh 2>&1 | tee train_$(date +%Y%m%d_%H%M%S).log"
