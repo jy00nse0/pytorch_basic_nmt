358ae1bd7413d80b30aa03016cac9e4bd7c4bd2e
Jang Yoon seo <123075825+jy00nse0@users.noreply.github.com>
2026-01-25 02:22:59 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ cd data
 pip install gdown
 gdown --fuzzy https://drive.google.com/file/d/17bmdohiXGQDd6DE1pDl0VvNV702K8uuG/view?usp=sharing
 tar -xvf wmt14_tok_len50.tar
-
+cd ../
 conda activate pytorch_nmt
-chmod +x train.sh
+chmod +x scripts/train.sh
 
 #tmux new-session -d -s nmt_train \
 #"bash scripts/train.sh 2>&1 | tee train_$(date +%Y%m%d_%H%M%S).log"
